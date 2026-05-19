6cee50b9857919cf26274f5195c3559215075b97
emmamalmberg1-arch <emmamalmberg1@gmail.com>
2026-05-19 13:40:00 +0200

    feat: create TemplateHeader component with HTML, CSS, and tests

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateHeaderComponent } from './components/template-header/template-header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TemplateHeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
