b65356c70e91cc54752a9bbf1b8097450cc40253
AdrianKebab <adrian.naslund05@gmail.com>
2026-05-19 13:48:24 +0200

    add empty page
    did the command under:
    ng generate component pages/student-page

---
+++ Client/src/app/pages/student-page/student-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-page',
+  imports: [],
+  templateUrl: './student-page.html',
+  styleUrl: './student-page.css',
+})
+export class StudentPage {}
