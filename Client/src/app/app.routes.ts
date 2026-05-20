b0473d982034fb8061da1a5a6c0c20d7f0d86d97
nadiiakomar <komarnadia21@gmail.com>
2026-05-20 10:08:59 +0200

    feat: implement courses page with routing, template header, and course table

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CoursesPage } from './pages/courses-page/courses-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'courses-page',
+        component: CoursesPage
+    }
+];
--- Client/src/app/pages/courses-page/courses-page.ts
+++ Client/src/app/pages/courses-page/courses-page.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TemplateHeaderComponent } from '../../components/template-header/template-header';
+
 
 @Component({
   selector: 'app-courses-page',
-  imports: [],
+  imports: [TemplateHeaderComponent, CommonModule],
   templateUrl: './courses-page.html',
   styleUrl: './courses-page.css',
 })
-export class CoursesPage {}
+export class CoursesPage {
+   courses = [
+    { name: 'C# Grund',
+       code: 'C#',
+     category: 'Databaser' },
+     { name: 'C# Grund',
+       code: 'C#',
+     category: 'Databaser' },
+     { name: 'C# Grund',
+       code: 'C#',
+     category: 'Databaser' }
+  ];
+}
