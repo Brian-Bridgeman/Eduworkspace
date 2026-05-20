8de3ddbeed63b5572d420441dcc0c25d231b4628
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2026-05-20 09:55:20 +0200

    chore: Routed start page component to be visible from start url

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StartPage } from './pages/start-page/start-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StartPage
+  }
+];
