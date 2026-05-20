f06a42364a4472e8556964de0c709849074985b2
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-05-20 14:22:50 +0200

    add student modal template and styling

---
+++ Client/src/app/components/add-student-modal/add-student-modal.ts
@@ -0,0 +1,10 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-add-student-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-student-modal.html',
+  styleUrl: './add-student-modal.css',
+})
+export class AddStudentModal { }
