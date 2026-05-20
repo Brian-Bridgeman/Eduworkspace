e10ee1b1e9ddca8cb198e34ac5640e64d4a7b76d
emmamalmberg1-arch <emmamalmberg1@gmail.com>
2026-05-20 14:53:31 +0200

    Add checklist functionality with dynamic item management and styling updates

--- Client/src/app/components/checklist-modal/checklist-modal.ts
+++ Client/src/app/components/checklist-modal/checklist-modal.ts
@@ -1,18 +1,47 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-checklist-modal',
-  standalone: true,
-  imports: [],
-  templateUrl: './checklist-modal.html',
-  styleUrl: './checklist-modal.css'
+    selector: 'app-checklist-modal',
+    standalone: true,
+
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+
+    templateUrl: './checklist-modal.html',
+    styleUrl: './checklist-modal.css'
 })
 export class ChecklistModal {
 
-  @Output() close = new EventEmitter<void>();
+    @Output() close = new EventEmitter<void>();
 
-  closeModal() {
-    this.close.emit();
-  }
+    checklistItems = [
+        {
+            text: '',
+            checked: false
+        }
+    ];
+
+    closeModal() {
+        this.close.emit();
+    }
+
+    addItem() {
+        this.checklistItems.push({
+            text: '',
+            checked: false
+        });
+    }
+
+    removeItem(index: number) {
+        this.checklistItems.splice(index, 1);
+    }
+
+    trackByIndex(index: number): number {
+        return index;
+    }
 
 }
