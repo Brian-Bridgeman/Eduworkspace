4b6866b2df6efd08643e8bd0a4c36098a263e010
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2026-05-20 09:52:10 +0200

    feat: created a start page component with basic text and containers

---
+++ Client/src/app/pages/start-page/start-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  imports: [],
+  templateUrl: './start-page.html',
+  styleUrl: './start-page.css',
+})
+export class StartPage {}
