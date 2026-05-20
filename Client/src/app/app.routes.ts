2e6c7586d2f7c355f0e075a86094495e3be2dd72
ViaNystrm <olivia.nystrom@hotmail.com>
2026-05-20 13:15:16 +0200

    Add router links for courses and students to sidebar

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { CoursesPage } from './pages/courses-page/courses-page';
 import { StudentPage } from './pages/student-page/student-page';
 
 export const routes: Routes = [
-    {path: 'courses-page',
+    {
+        path: 'courses',
         component: CoursesPage
     },
     {
-    path: 'student-page',
-    component: StudentPage
+        path: 'students',
+        component: StudentPage
     }
 ];
