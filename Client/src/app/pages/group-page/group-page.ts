6a0d766822082bac69c7c2c5a325f279c0aa7da7
AdrianKebab <adrian.naslund05@gmail.com>
2026-05-20 13:35:21 +0200

    add empty page with command
    ng generate component pages/namn

---
+++ Client/src/app/pages/group-page/group-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-page',
+  imports: [],
+  templateUrl: './group-page.html',
+  styleUrl: './group-page.css',
+})
+export class GroupPage {}
