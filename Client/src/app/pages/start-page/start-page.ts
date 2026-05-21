2e6806f697ea7eb8c0f26ddebe2de09ca3ecdf45
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2026-05-21 10:32:25 +0200

    Feat: Created a start-page

--- Client/src/app/pages/start-page/start-page.ts
+++ Client/src/app/pages/start-page/start-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { StartPageCard } from '../../components/start-page-card/start-page-card';
 
 @Component({
   selector: 'app-start-page',
-  imports: [],
+  imports: [StartPageCard],
   templateUrl: './start-page.html',
   styleUrl: './start-page.css',
 })
