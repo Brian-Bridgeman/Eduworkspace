908a419c32275dec58d810a2becb9aff7b24c563
Joanna Stendahl <joannastendahl@gmail.com>
2026-05-21 11:21:38 +0200

    Lagt in komponenter

---
+++ Client/src/app/components/kalender/kalender.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kalender',
+  imports: [],
+  templateUrl: './kalender.html',
+  styleUrl: './kalender.css',
+})
+export class Kalender {}
