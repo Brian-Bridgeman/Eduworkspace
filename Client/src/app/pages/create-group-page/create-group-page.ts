888dba0dc43c049c69c4e35b73deda0c5fe28dbb
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-05-21 12:55:44 +0200

    create group template and styling

---
+++ Client/src/app/pages/create-group-page/create-group-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-group-page',
+  imports: [],
+  templateUrl: './create-group-page.html',
+  styleUrl: './create-group-page.css',
+})
+export class CreateGroupPage {}
