55ac51e62ee120b9cac9bb43c8d5a5dc6428638e
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-05-21 13:22:44 +0200

    open add student modal

--- Client/src/app/pages/create-group-page/create-group-page.ts
+++ Client/src/app/pages/create-group-page/create-group-page.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { AddStudentModal } from '../../components/add-student-modal/add-student-modal';
 
 @Component({
   selector: 'app-create-group-page',
-  imports: [],
+  imports: [AddStudentModal],
   templateUrl: './create-group-page.html',
   styleUrl: './create-group-page.css',
 })
-export class CreateGroupPage {}
+
+export class CreateGroupPage {
+  showAddStudentModal = false;
+}
