481d0094fba5a67a4603238f86fbfcd795014c20
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2026-05-21 10:44:02 +0200

    Feat: Removed red border and fixed page routing

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CoursesPage } from './pages/courses-page/courses-page';
 import { StudentPage } from './pages/student-page/student-page';
 import { ChecklistModal } from './components/checklist-modal/checklist-modal';
+import { StartPage } from './pages/start-page/start-page';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
         path: 'groups',
         component: GroupPage
     },*/
+  {
+    path: 'start',
+    component: StartPage,
+  },
   {
     /* ÄNDRA HÄR NÄR RESPEKTIVE SIDOR FINNS */
     path: 'classes',
