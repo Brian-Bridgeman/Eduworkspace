223533d74eae69061e97856b811ed18faa23d5b8
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2026-05-22 00:07:24 +0200

    Feat: Added start-page conditional CSS to app.css

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,17 +1,35 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 import { Header } from './components/header/header';
 import { CommonModule } from '@angular/common';
 import { ChecklistModal } from './components/checklist-modal/checklist-modal';
+import { filter } from 'rxjs';
+import { HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Sidebar, Header, CommonModule, ChecklistModal],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Client');
   showModal = false;
+
+  showSidebar = true;
+
+  @HostBinding('class.start-route')
+  isStartRoute = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const isStart = event.url === '/start';
+
+        this.isStartRoute = isStart;
+        this.showSidebar = !isStart;
+      });
+  }
 }
