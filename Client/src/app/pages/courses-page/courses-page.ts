0fe3ed4740226d8c04eda70bdb470c5072e559e0
AdrianKebab <adrian.naslund05@gmail.com>
2026-05-21 14:54:54 +0200

    add routerlink to courses-page

--- Client/src/app/pages/courses-page/courses-page.ts
+++ Client/src/app/pages/courses-page/courses-page.ts
@@ -2,28 +2,33 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateHeaderComponent } from '../../components/template-header/template-header';
 import { DropdownMenu } from '../../components/dropdown-menu/dropdown-menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-courses-page',
-  imports: [TemplateHeaderComponent, CommonModule, DropdownMenu],
+  imports: [TemplateHeaderComponent, RouterModule, CommonModule, DropdownMenu],
   templateUrl: './courses-page.html',
   styleUrl: './courses-page.css',
 })
+
 export class CoursesPage {
   searchTerm: string = '';
 
   courses = [
     {
+      id: '1',
       name: 'C# Grund',
       code: 'C#',
       category: 'Databaser'
     },
     {
+      id: '2',
       name: 'Fiber Grund',
       code: 'PHP',
       category: 'Frontend'
     },
     {
+      id: '3',
       name: 'Kebab Grund',
       code: 'Python',
       category: 'Backend'
