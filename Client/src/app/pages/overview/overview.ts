43f94e64bb90959276499d79b731c51d297fbde1
nadiiakomar <komarnadia21@gmail.com>
2026-05-22 08:53:07 +0200

    feat: create overview dashboard page with ongoing and upcoming groups

--- Client/src/app/pages/overview/overview.ts
+++ Client/src/app/pages/overview/overview.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { DropdownMenu } from '../../components/dropdown-menu/dropdown-menu';
+import { TemplateHeaderComponent } from '../../components/template-header/template-header';
+import { RouterLink } from '@angular/router';
+import { routes } from '../../app.routes';
 @Component({
   selector: 'app-overview',
-  standalone:true,
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, DropdownMenu, TemplateHeaderComponent, RouterLink],
   templateUrl: './overview.html',
   styleUrl: './overview.css',
 })
-export class Overview {}
+export class Overview {
+
+  ongoingGroups = [
+    {
+      groupName: 'Oscar & Johan',
+      course: 'Elinstallation',
+      group: 'Grupp A',
+      location: 'Sal B',
+      status: 'Pågående',
+      participants: [
+        {name: 'Oscar'},
+        {name: 'Johan'}]
+    },
+
+    {
+      groupName: 'Kalle & Alfred',
+      course: 'Webbutveckling',
+      group: 'Grupp B',
+      location: 'Sal C',
+      status: 'Pågående',
+      participants: [
+        {name: 'Kalle'},
+        {name: 'Alfred'}]
+    }
+  ];
+
+  upcomingGroups = [
+    {
+      groupName: 'Simon & Hugo',
+      course: 'Industrisäkerhet',
+      group: 'Grupp B',
+      location: 'Sal E',
+      status: 'Kommande',
+      participants: [
+        {name: 'Simon'},
+        {name: 'Hugo'}]
+    }
+  ];
+
+  latestNotes = [
+    {
+      date: '2026-05-20',
+      group: 'Oscar & Johan',
+      note: 'Kontroll av utrustning genomförd.',
+      createdBy: 'Johan'
+    }
+  ];
+}
