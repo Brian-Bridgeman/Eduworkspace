f9f57e9aecf8c5aff4133aa64c408607ea571462
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2026-05-22 02:13:47 +0200

    Chore: Fixed start page to match website theme

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -6,16 +6,13 @@ import { CommonModule } from '@angular/common';
 import { ChecklistModal } from './components/checklist-modal/checklist-modal';
 import { filter } from 'rxjs';
 import { HostBinding } from '@angular/core';
-import { ChecklistSetCard } from './components/checklist-set-card/checklist-set-card';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, Sidebar, Header, CommonModule, ChecklistModal, ChecklistSetCard],
+  imports: [RouterOutlet, Sidebar, Header, CommonModule, ChecklistModal],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-
 export class App {
   protected readonly title = signal('Client');
   showModal = false;
@@ -34,11 +31,5 @@ export class App {
         this.isStartRoute = isStart;
         this.showSidebar = !isStart;
       });
-
-  checklists = signal<any[]>([]);
-  showModal = signal(false);
-
-  addChecklist(checklist: any) {
-    this.checklists.update(list => [...list, checklist]);
   }
 }
