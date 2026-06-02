cf7eaa3b83101a0679058d48a5269fb84d7376fe
nadiiakomar <komarnadia21@gmail.com>
2026-06-02 10:56:49 +0200

    feat: add Company  model and configure AppDbContext for Company

--- Server/Data/AppDbContext.cs
+++ Server/Data/AppDbContext.cs
@@ -8,4 +8,5 @@ public class AppDbContext: DbContext
         public DbSet <User> Users {get;set;}
         public DbSet <Role> Roles {get;set;}
         public DbSet <Course> Courses {get;set;}
+        public DbSet <Company> Company {get;set;}
 }
