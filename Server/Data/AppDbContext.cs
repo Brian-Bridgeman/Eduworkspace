4942aa73f66a0c26c1a346bee5ba0e438b296f4e
nadiiakomar <komarnadia21@gmail.com>
2026-06-02 10:50:46 +0200

    feat: add Roles model and configure AppDbContext for Roles

--- Server/Data/AppDbContext.cs
+++ Server/Data/AppDbContext.cs
@@ -6,4 +6,5 @@ public class AppDbContext: DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {}
         public DbSet <User> Users {get;set;}
+        public DbSet <Roles> Roles {get;set;}
 }
