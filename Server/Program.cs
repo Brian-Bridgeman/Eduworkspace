2b778038e2e7058afccacc68019df28ddb050d9f
nadiiakomar <komarnadia21@gmail.com>
2026-06-02 09:42:13 +0200

    feat: add AppDbContext and configure SQLite database connection

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source=app.db");
+});
