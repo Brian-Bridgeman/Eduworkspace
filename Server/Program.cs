fc74161ea6f844ec6850fa146c198165f522a971
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-06-02 11:44:41 +0200

    configure nswag

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,7 +2,15 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
     WebRootPath = "wwwroot/browser"
 });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
