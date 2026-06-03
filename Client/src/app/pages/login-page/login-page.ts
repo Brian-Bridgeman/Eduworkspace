fef9cbe103ebb2504691920c1ed6e5cac5e705d9
ViaNystrm <olivia.nystrom@hotmail.com>
2026-06-03 14:34:26 +0200

    Add ngModel bindings and import formsmodule

--- Client/src/app/pages/login-page/login-page.ts
+++ Client/src/app/pages/login-page/login-page.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+
+  username = '';
+  password = '';
 
   constructor(private router: Router) { }
 
