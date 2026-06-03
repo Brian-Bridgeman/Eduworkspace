01e1e5b250bc821bfe62f959b03c50184552c27a
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2026-06-03 11:27:55 +0200

    Feat: Added client to providers in app.config.ts

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -1,11 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { Client } from './services/api-client.service';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), Client],
 };
