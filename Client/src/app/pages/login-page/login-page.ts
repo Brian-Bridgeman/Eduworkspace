89fed7b631f7fd9356704fc2826b547824c3115e
ViaNystrm <olivia.nystrom@hotmail.com>
2026-06-03 14:12:10 +0200

    Create login page and routing to start page

--- Client/src/app/pages/login-page/login-page.ts
+++ Client/src/app/pages/login-page/login-page.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
+  standalone: true,
   imports: [],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+
+  constructor(private router: Router) { }
+
+  login() {
+    this.router.navigate(['/start']);
+  }
+}
